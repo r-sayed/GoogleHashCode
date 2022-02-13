d8e9948ce7218eb37c8912cdf55d8f937a1f044a
Mohammed Rehan <rsayed@intelinotion.com>
2022-02-13 08:56:21 +0530

    Added code for hash code problem 2021 qulification round

---
+++ 2021/2021QualificationRound/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2021QualificationRound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
